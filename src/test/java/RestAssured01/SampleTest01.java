5112aa7fb9ec471bcc30419f37cecfd28f62900f
Irfan Khan <irfan.ahmadkhan@epam.com>
2023-04-19 14:22:43 +0530

    added test004

--- src/test/java/RestAssured01/SampleTest01.java
+++ src/test/java/RestAssured01/SampleTest01.java
@@ -13,11 +13,15 @@ public class SampleTest01 {
 	@Test
 	public void Test002()
 	{
-		System.out.println("This is test001");
+		System.out.println("This is test002");
 	}
 	@Test
 	public void Test003()
 	{
-		System.out.println("This is test001");
+		System.out.println("This is test002");
+	}
+	public void Test004()
+	{
+		System.out.println("This is test002");
 	}
 }
